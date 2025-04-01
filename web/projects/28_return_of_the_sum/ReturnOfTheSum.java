213c87a31deb89e22a1ec8dedd1f17ce1f85f9ca
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-04-01 01:21:47 -0300

    feat: Created a new program

---
+++ web/projects/28_return_of_the_sum/ReturnOfTheSum.java
@@ -0,0 +1,10 @@
+public class ReturnOfTheSum {
+    public static void main(String[] args) {
+        int resultado = soma(6, 4, 3);
+        System.out.println("A soma é: " + resultado);
+    }
+
+    public static int soma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
