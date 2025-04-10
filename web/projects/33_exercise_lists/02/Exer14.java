b7efd22204a03062e4fca81f344ee142728bc329
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-04-10 01:11:17 -0300

    feat: Created new programs

--- web/projects/33_exercise_lists/02/Exer14.java
+++ web/projects/33_exercise_lists/02/Exer14.java
@@ -5,8 +5,40 @@ public class Exer14 {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Entre com um número:");
-        int num = scan.nextInt();
+        System.out.print("Entre com a primeira nota: ");
+        double nota01 = scan.nextDouble();
+
+        System.out.print("Entre com a segunda nota: ");
+        double nota02 = scan.nextDouble();
+
+        double media = (nota01 + nota02) / 2;
+
+        String aproveitamento = "";
+        if (media >= 9 && media <= 10) {
+            aproveitamento = "A";
+        } else if (media >= 7.5 && media < 9) {
+            aproveitamento = "B";
+        } else if (media >= 6 && media < 7.5) {
+            aproveitamento = "C";
+        } else if (media >= 4 && media < 6) {
+            aproveitamento = "D";
+        } else {
+            aproveitamento = "E";
+        }
+
+        System.out.println("Primeira nota: " + nota01);
+        System.out.println("Segunda nota: " + nota02);
+        System.out.println("Média: " + media);
+        System.out.println("Conceito: " + aproveitamento);
+
+        switch (aproveitamento) {
+            case "A":
+            case "B":
+            case "C": System.out.println("Aprovado!"); break;
+            case "D":
+            case "E": System.out.println("Reprovado!"); break;
+
+        }
 
         scan.close();
     }
