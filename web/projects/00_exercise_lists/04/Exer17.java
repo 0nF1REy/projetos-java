3a2918a7c2de5697991dc3e3b274e2349ab7e2df
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-05-27 00:16:59 -0300

    feat: Created new programs

---
+++ web/projects/00_exercise_lists/04/Exer17.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exer17 {
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] idades = new int[10];
+
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println("Entre com a idade da pessoa " + (i+1));
+            idades[i] = scan.nextInt();
+        }
+
+        int qtd = 0;
+
+        for (int i = 0; i < idades.length; i++) {
+            if (idades[i] > 35) {
+                qtd++;
+            }
+        }
+
+        System.out.print("Vetor de idades = ");
+        for (int i = 0; i < idades.length; i++) {
+            System.out.print(idades[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Quantidade de pessoas com idade > 35: " + qtd);
+    }
+}
