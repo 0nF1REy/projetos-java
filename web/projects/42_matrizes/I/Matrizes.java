5adcd856ad083530f926386b7cd02c9a7dc2d58d
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-06-08 01:24:23 -0300

    feat: Created new programs

--- web/projects/42_matrizes/I/Matrizes.java
+++ web/projects/42_matrizes/I/Matrizes.java
@@ -4,38 +4,27 @@ public class Matrizes {
         // Matriz 3x4
         double[][] notasAlunos = new double[3][4];
 
-        // ===================
         // Preenchimento Manual
-        // ===================
-        // Vetores (linhas da matriz)
-        // Vetor 1
         notasAlunos[0][0] = 10;
         notasAlunos[0][1] = 7;
         notasAlunos[0][2] = 9;
         notasAlunos[0][3] = 9.5;
 
-        // Vetor 2
         notasAlunos[1][0] = 9;
         notasAlunos[1][1] = 8;
         notasAlunos[1][2] = 7;
         notasAlunos[1][3] = 9;
 
-        // Vetor 3
         notasAlunos[2][0] = 8;
         notasAlunos[2][1] = 9;
         notasAlunos[2][2] = 10;
         notasAlunos[2][3] = 7;
 
-        // ==========================================
         // Declaração de arrays unidimensionais (vetores)
-        // ==========================================
-
         double[] notasAluno1 = {7, 8, 9, 10};
         double[] notasAluno2 = {8, 6, 7, 10};
 
-        // ========================
-        // Impressão da matriz inicial
-        // ========================
+        // Impressão da matriz inicia
         System.out.println("Matriz notasAlunos:");
         for (int i = 0; i < notasAlunos.length; i++) {
             System.out.print("Aluno " + (i + 1) + ": ");
@@ -45,14 +34,10 @@ public class Matrizes {
             System.out.println();
         }
 
-        // ======================
         // Alteração de um valor
-        // ======================
         notasAlunos[1][3] = 8;
 
-        // ===========================
         // Impressão após modificação
-        // ===========================
         System.out.println("\nMatriz atualizada (nota final do aluno 2 alterada para 8):");
         for (int i = 0; i < notasAlunos.length; i++) {
             System.out.print("Aluno " + (i + 1) + ": ");
@@ -62,9 +47,7 @@ public class Matrizes {
             System.out.println();
         }
 
-        // =========================
         // Cálculo da média por aluno
-        // =========================
         System.out.println("\nMédia de cada aluno:");
         for (int i = 0; i < notasAlunos.length; i++) {
             double soma = 0;
