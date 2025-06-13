5b737b85b6158eda1a6d54c2dfcd7dde117d127d
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-06-13 00:11:17 -0300

    feat: Created new programs

--- web/projects/44_carro/carro/Carro.java
+++ web/projects/44_carro/carro/Carro.java
@@ -17,4 +17,10 @@ public class Carro {
         System.out.println("Método obterAutonomia foi chamado.");
         return capCombustivel * consumoCombustivel;
     }
+
+    // Método com parâmetro/argumento
+    double calcularCombustivel(double km) {
+        double qtdCombustivel = km / consumoCombustivel;
+        return qtdCombustivel;
+    }
 }
