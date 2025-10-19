d07f3394fab15512810755a43354b52105c824ee
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-10-19 16:09:21 -0300

    master: feat (gui/java-fx) - Added "JavaFX" application module
    - Created new `gui/java-fx` module with Maven setup and dependencies for JavaFX, ControlsFX, FormsFX, ValidatorFX, Ikonli, BootstrapFX, TilesFX, and FXGL.
    - Added main application class, controller, FXML view, and resources (icon).
    - Include build scripts and configuration files.

---
+++ gui/java-fx/src/main/java/br/com/alanryan/javafx/hello_fx/HelloFX.java
@@ -0,0 +1,39 @@
+package br.com.alanryan.javafx.hello_fx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        // Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image(getClass().getResourceAsStream("/br/com/alanryan/javafx/hello_fx/icon.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Stage Demo Program w00t w00t");
+        stage.setWidth(420);
+        stage.setHeight(420);
+        stage.setResizable(false);
+        // stage.setX(50);
+        // stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
