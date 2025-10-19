3958ff313a1066ceb2caaadddc2079a8af376420
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-10-19 15:12:38 -0300

    master: chore - Reorganized project structure
    - Moved all source files from `src/` to `cli/src/`, keeping the organization by exercises, examples, and modules.
    - Removed the `java-fx-applications` module and all its related files (Maven, Java, FXML, resources).
    - Added new entry point file `cli/src/Main.java`.

--- src/java-fx-applications/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxapplications {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxapplications to javafx.fxml;
-    exports com.example.javafxapplications;
-}
