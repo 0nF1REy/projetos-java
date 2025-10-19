8d2e227cff9d0f845599357d79baeb1947354381
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-10-19 15:17:43 -0300

    master: feat (Main) - Implemented directory listing with last modified timestamps

--- cli/src/Main.java
+++ cli/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.text.SimpleDateFormat;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Diretório atual (onde o programa está sendo executado)
+        File currentDir = new File(System.getProperty("user.dir"));
+
+        System.out.println("Diretório atual: " + currentDir.getAbsolutePath());
+        System.out.println("----------------------------------------------------------");
+        System.out.printf("%-5s %-40s %-20s%n", "#", "Nome da Pasta", "Última Modificação");
+        System.out.println("----------------------------------------------------------");
+
+        // Obtém lista de arquivos e diretórios
+        File[] files = currentDir.listFiles();
+        if (files == null) {
+            System.out.println("Nenhum arquivo encontrado.");
+            return;
         }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        int index = 1;
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                String lastModified = sdf.format(file.lastModified());
+                System.out.printf("%-5d %-40s %-20s%n", index++, file.getName(), lastModified);
+            }
+        }
+
+        System.out.println("----------------------------------------------------------");
+        System.out.printf("Total de pastas: %d%n", (index - 1));
     }
 }
