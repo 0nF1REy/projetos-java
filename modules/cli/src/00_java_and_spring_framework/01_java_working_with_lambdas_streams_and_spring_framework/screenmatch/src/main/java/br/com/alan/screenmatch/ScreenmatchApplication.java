0fccdcf1ae4a0eb2adc6042bb798390e38972b77
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-22 01:21:32 -0300

    main: feat (screenmatch) - Added data model and API consumption services; Updated build
    - Extended the Screenmatch demo by adding domain models and service implementations for API consumption and data conversion, and updated the module build configuration and application wiring.

--- modules/cli/src/00_java_and_spring_framework/01_java_working_with_lambdas_streams_and_spring_framework/screenmatch/src/main/java/br/com/alan/screenmatch/ScreenmatchApplication.java
+++ modules/cli/src/00_java_and_spring_framework/01_java_working_with_lambdas_streams_and_spring_framework/screenmatch/src/main/java/br/com/alan/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,32 @@
 package br.com.alan.screenmatch;
 
+import br.com.alan.screenmatch.models.DadosSerie;
+import br.com.alan.screenmatch.services.ConsumoApi;
+import br.com.alan.screenmatch.services.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d316c8a0");
+
+		System.out.println(json);
+
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
 }
