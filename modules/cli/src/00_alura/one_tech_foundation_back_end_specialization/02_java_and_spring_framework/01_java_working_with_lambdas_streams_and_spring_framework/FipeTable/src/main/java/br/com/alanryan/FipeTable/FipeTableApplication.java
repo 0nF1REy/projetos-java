50cb07b27fd7d0c135e12739e6e40cea11a84c15
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-23 18:17:40 -0300

    main: feat (FipeTable) - Implemented "FipeTable" application with API consumption and data conversion features

--- modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/01_java_working_with_lambdas_streams_and_spring_framework/FipeTable/src/main/java/br/com/alanryan/FipeTable/FipeTableApplication.java
+++ modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/01_java_working_with_lambdas_streams_and_spring_framework/FipeTable/src/main/java/br/com/alanryan/FipeTable/FipeTableApplication.java
@@ -1,13 +1,20 @@
 package br.com.alanryan.FipeTable;
 
+import br.com.alanryan.FipeTable.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FipeTableApplication {
+public class FipeTableApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FipeTableApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
 }
---
+++ modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/01_java_working_with_lambdas_streams_and_spring_framework/FipeTable/src/main/java/br/com/alanryan/FipeTable/models/Dados.java
@@ -0,0 +1,4 @@
+package br.com.alanryan.FipeTable.models;
+
+public record Dados(String codigo, String nome) {
+}
