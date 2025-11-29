0b5f8ab501244ca3ef561e53b2c32d09e6788a3a
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-29 15:22:32 -0300

    main: fix (02_practicing_java_collections_and_streams) - Updated datetime practice app entrypoint
    - Adjusted the datetime practice application's main entrypoint and perform minor cleanup to improve startup clarity and maintainability.

--- modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/02_practicing_java_collections_and_streams/datetime/src/br/com/alanryan/datetime/app/Principal.java
+++ modules/cli/src/00_alura/one_tech_foundation_back_end_specialization/02_java_and_spring_framework/02_practicing_java_collections_and_streams/datetime/src/br/com/alanryan/datetime/app/Principal.java
@@ -3,31 +3,49 @@ package br.com.alanryan.datetime.app;
 import java.util.*;
 
 public class Principal {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Principal app = new Principal();
+
+        app.demoList();
+        System.out.println("- - -");
+        app.demoSet();
+        System.out.println("- - -");
+        app.demoMap();
+    }
+
+    // Demonstração de List
+    private void demoList() {
         List<String> funcionarios = new ArrayList<>();
         funcionarios.add("Alan");
         funcionarios.add("Sarah");
         funcionarios.add("Stella");
 
+        System.out.println("Lista de funcionários:");
         System.out.println(funcionarios);
-        System.out.println("- - -");
-        System.out.println(funcionarios.get(1));
-        System.out.println("- - -");
+        System.out.println("Segundo funcionário: " + funcionarios.get(1));
+    }
 
+    // Demonstração de Set
+    private void demoSet() {
         Set<String> produtos = new HashSet<>();
         produtos.add("Água");
         produtos.add("Coca-cola");
         produtos.add("Água");
 
+        System.out.println("Conjunto de produtos:");
         System.out.println(produtos);
+    }
 
+    // Demonstração de Map
+    private void demoMap() {
         Map<Integer, String> clientes = new HashMap<>();
         clientes.put(1, "Maria");
         clientes.put(2, "Marcos");
         clientes.put(3, "Elaine");
 
+        System.out.println("Mapa de clientes:");
         System.out.println(clientes);
-        System.out.println(clientes.get(3));
+        System.out.println("Cliente com chave 3: " + clientes.get(3));
     }
 }
